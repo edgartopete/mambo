f283891c9aea44bf17a217f457b7f9a36ee841d7
et code <edgar.topete@gmail.com>
2019-05-01 13:07:27 -0500

    updates to storefront, create service, create account

---
+++ assets/js/create_acount.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $("#register").on("click", function () {
+        $("#createAccount").modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+    $("#create").on("click", function(event){
+        event.preventDefault();
+        var user=null;
+        var email= $("#email").val();
+        var pass =$("#password").val();
+        var name = $("#name").val();
+        var last = $("#last").val();
+        var gender = $("#gender").val();
+        var zip = $("#zip").val();
+
+        //create account in firebase
+
+        firebase.auth().createUserWithEmailAndPassword(email, pass)
+        .then(function(){
+            user = firebase.auth().currentUser;
+        }).then(function() {
+            user.updateProfile({
+                displayName:userDisplay
+            });
+        });
+
+
+        if ( user !== "") {
+          
+
+            var newUser = {
+                name:name,
+                last:last,
+                gender:gender,
+                zip:zip
+            }
+            
+            mambodb.ref("/user").push(newUser);
+           
+        } else {
+            console.log("error");
+        }
+    });
+
+    $("#btnlogin2").on("click",function(event){
+        event.preventDefault();
+        var email=$("#inputEmail").val();
+        var password=$("#inputPassword").val();
+       console.log("entro");
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorMessage = error.message;
+
+            console.log(errorMessage);
+            // ...
+          });
+    });
+});
