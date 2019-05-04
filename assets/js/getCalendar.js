08028120ef8b168640f68936cdc88adaefd5981b
edgartopete <46701054+edgartopete@users.noreply.github.com>
2019-05-03 21:18:05 -0500

    Update getCalendar.js

--- assets/js/getCalendar.js
+++ assets/js/getCalendar.js
@@ -9,12 +9,11 @@ $(document).ready(function () {
     // Authorization scopes required by the API; multiple scopes can be
     // included, separated by spaces.
     var SCOPES = "https://www.googleapis.com/auth/calendar";
-    //var authorizeButton = document.getElementById('authorize_button');
-    //var signoutButton = document.getElementById('signout_button');
+    
     var date;
     var maxDate;
     var timesNot = [];
-    var fromTime = ""
+    var fromTime = "";
     /*get the variables to check times available*/
     $("#getDates").on("click", function (event) {
         event.preventDefault();
@@ -43,17 +42,9 @@ $(document).ready(function () {
             $("#msg").show();
             $("#msg").text("Plase fill all the fills.");
         }
-
-
-
     }
 
-    /**
-     *  On load, called to load the auth2 library and API client library.
-     */
-    function handleClientLoad() {
-        //gapi.load('client:auth2', initClient);
-    }
+  
     /**
      *  Initializes the API client library and sets up sign-in state
      *  listeners.
@@ -67,12 +58,12 @@ $(document).ready(function () {
         }).then(function () {
             // Listen for sign-in state changes.
             gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+            
             // Handle the initial sign-in state.
             updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-            //authorizeButton.onclick = handleAuthClick;
-            //signoutButton.onclick = handleSignoutClick;
+            
         }, function (error) {
-            appendPre(JSON.stringify(error, null, 2));
+            console.log(error);
         });
     }
     /**
@@ -96,17 +87,7 @@ $(document).ready(function () {
     function handleSignoutClick(event) {
         gapi.auth2.getAuthInstance().signOut();
     }
-    /**
-     * Append a pre element to the body containing the given message
-     * as its text node. Used to display the results of the API call.
-     *
-     * @param {string} message Text to be placed in pre element.
-     */
-    function appendPre(message) {
-        var pre = document.getElementById('content');
-        var textContent = document.createTextNode(message + '\n');
-        pre.appendChild(textContent);
-    }
+    
     /**
      * Print the summary and start datetime/date of the next ten events in
      * the authorized user's calendar. If no events are found an
@@ -141,7 +122,6 @@ $(document).ready(function () {
                 console.log(timesNot);
                 printTimes();
             } else {
-                //appendPre('No upcoming events found.');
                 printTimes();
             }
         });
@@ -163,7 +143,6 @@ $(document).ready(function () {
                     case 'm':
                         for (i = 10; i < 13; i++) {
                             if (!timesNot.includes(i.toString())) {
-                                //console.log(i);
                                 createButtons(i);
                             }
                         }
@@ -171,7 +150,7 @@ $(document).ready(function () {
                     case 'n':
                         for (i = 13; i < 21; i++) {
                             if (!timesNot.includes(i.toString())) {
-                                //console.log(i);
+                            
                                 createButtons(i);
                             }
                         }
@@ -193,6 +172,8 @@ $(document).ready(function () {
             }
         }
     }
+    
+    //click event for buttons created
     $("body").on("click", ".toShedule", function () {
         var startTime = $(this).text();
         var startdate = $("#datepicker").val();
@@ -237,7 +218,7 @@ $(document).ready(function () {
             // handle the response from our api call
             request.execute(function (resp) {
               if (resp.status == 'confirmed') {
-                //document.getElementById('event-response').innerHTML = "Event created successfully. View it <a href='" + resp.htmlLink + "'>online here</a>.";
+                
                 $("#msg").removeClass("alert-danger");
                 $("#msg").addClass("alert-success");
                 $("#msg").show();
@@ -245,8 +226,7 @@ $(document).ready(function () {
                 $("#scheduleForm").empty();
     
               } else {
-                //document.getElementById('event-response').innerHTML = "There was a problem. Reload page and try again.";
-                //console.log("error intente de nuevo ");
+            
                 $("#msg").show();
                 $("#msg").text("Somethis when wrong. Please try in a few minutes");
               }
